674f470a8da4f4e3bd9dbff8ffa36ca806116f7c
epicsharkbait8 <epicsharkbait82@gmail.com>
2025-02-25 19:44:46 +0000

    lets go gambling!

---
+++ gamblinghedahh.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+def generate_wheel():
+    spaces = []
+    for i in range(18):
+        spaces.append("red")
+        spaces.append("black")
+    for i in range(2):
+        spaces.append("green")
+    return spaces
+
+
+
+def spin_wheel(spaces):
+    return random.choice(spaces)
+
+
+def play_game():
+    money = 1000
+    spaces = generate_wheel()
+
+    while True:
+        print("you have $" + str(money) + " to gamble.")
+        bet = input("How much you betting? ")
+        bet = int(bet)
+        color = input("What color do you bet on?")
+
+        print("The wheel is spinning, Good luck!")
+        time.sleep(3)
+        landed = spin_wheel(spaces)
+        print("It landed on " + landed + ".")
+
+        if landed == color:
+            money = money + bet
+            print("Yatta! you now got $" + str(money))
+        else:
+            money = money - bet
+            print("HAHAHAHA IMAGINE LOSING, now get lost, you now got $" + str(money))
+
+        play_again = input("wanna play again?")
+        if play_again == "no":
+            break
+play_game()
