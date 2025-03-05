3ed8acb5a472c126574e5fb076e59c8c690764b4
epicsharkbait8 <epicsharkbait82@gmail.com>
2025-03-05 06:21:49 +0000

    yatta done

---
+++ menuheadahh.py
@@ -0,0 +1,6 @@
+menu = { "Pizza": 1.99, "Soda":  0.69, "Double Chunk Chocolate Chip Cookie": 2.49}
+def addthing(thing, price):
+    menu[thing] = price
+addthing("Lime Jarrito", 1.99)
+
+print(menu)
